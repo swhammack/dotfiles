27aaa0aa1bb32b7ca62ef7ddd4ff1a754dba4417
Sean Hammack <swhammack@gmail.com>
2025-06-13 13:36:16 -0400

    Remove zim install. It is now done before chezmoi

--- run_once_before_install-packages.sh
+++ run_once_before_install-packages.sh
@@ -4,4 +4,3 @@ mise use -g zoxide
 mise use -g eza
 mise use -g fzf
 curl -s https://ohmyposh.dev/install.sh | bash -s
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
